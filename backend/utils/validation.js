838665cbe1199ab2d604903071c52c95381f6ba0
Ryou <nishiyamaryou@gmail.com>
2024-01-25 19:07:32 -0500

    validator obj check

---
+++ backend/utils/validation.js
@@ -0,0 +1,28 @@
+// backend/utils/validation.js
+const { validationResult } = require('express-validator');
+//package that gathers errors that were generated during the various checks and save them to an array
+
+
+// middleware for formatting errors from express-validator middleware
+// (to customize, see express-validator's documentation)
+const handleValidationErrors = (req, _res, next) => {
+  const validationErrors = validationResult(req);
+
+  if (!validationErrors.isEmpty()) {
+    const errors = {};
+    validationErrors
+      .array()
+      .forEach(error => errors[error.path] = error.msg);
+//form the error into an obj
+    const err = Error("Bad request.");
+    err.errors = errors;
+    err.status = 400;
+    err.title = "Bad request.";
+    next(err);
+  }
+  next();
+};
+
+module.exports = {
+  handleValidationErrors
+};
