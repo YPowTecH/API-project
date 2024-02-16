4682365e27c46c3267776807c708de8ea0f80052
Ryou <nishiyamaryou@gmail.com>
2024-02-15 20:38:39 -0500

    first redux day commit, auth me setup part1

---
+++ frontend/src/store/session.js
@@ -0,0 +1,55 @@
+//contain actions specific to the session user's info and session user's redux reducer
+//The csrfFetch is used to wrap the browser's fetch function, read the value of the XSRF-Token cookie, and add it to a header in the fetch request.
+import { csrfFetch } from './csrf';
+
+const SET_USER = "session/setUser";
+const REMOVE_USER = "session/removeUser";
+
+const setUser = (user) => {
+  return {
+    type: SET_USER,
+    payload: user
+  };
+};
+
+const removeUser = () => {
+  return {
+    type: REMOVE_USER
+  };
+};
+
+export const restoreUser = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+export const login = (user) => async (dispatch) => {
+  const { credential, password } = user;
+  const response = await csrfFetch("/api/session", {
+    method: "POST",
+    body: JSON.stringify({
+      credential,
+      password
+    })
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_USER:
+      return { ...state, user: action.payload };
+    case REMOVE_USER:
+      return { ...state, user: null };
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
