e5c30e8a1e99c36ae49968643ce2adbd1ab4f3df
Ryou <nishiyamaryou@gmail.com>
2024-02-19 14:08:21 -0500

    working on data showing for tiles

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,16 +1,29 @@
 import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { thunkLoadSpots } from "../../store/spots"
+
 import './LandingPage.css'
 
 function LandingPage() {
+    const dispatch = useDispatch()
+    const spotsList = useSelector((state)=>Object.values(state.spots))
+    console.log('my spots', spotsList)
+
+
+    useEffect(()=>{
+        dispatch(thunkLoadSpots())
+    }, [dispatch])
     return (
         <>
             <div className="tile-container">
-                <div className="tile">
+                {spots.map((spot)=>(
+                    <div className="tile" title={spot.name}>
                     <img></img>
                     <div className="review">stars</div>
                     <div className="location">city, state</div>
                     <div className="price">price</div>
                 </div>
+                ))}
             </div>
         </>
     )
--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -1,7 +1,41 @@
-import { csrfFetch } from "./csrf";
+import { createStore, combineReducers, applyMiddleware} from 'redux'
+import thunk from "redux-thunk";
+
+//constant
+const ALL_SPOTS = '/spots/ALL_SPOTS'
+
 
 //Action Creators
+const loadSpots = (spots) =>{
+    return{
+        type: ALL_SPOTS,
+        spots
+    }
+}
 
 //Thunks
+export const thunkLoadSpots = () => async(dispatch)=>{
+    const response = await fetch('/api/spots')
+
+    if(response.ok){
+        const data = await response.json()
+
+        dispatch(loadSpots(data))
+        return data
+    }
+}
 
 //Reducers
+const spotsReducer = (state = {}, action)=>{
+    switch(action.type){
+        case ALL_SPOTS:{
+            const newState={}
+            action.spots.Spots.forEach((spot)=>(newState[spot.id]=spot))
+            return newState
+        }
+        default:
+            return state
+    }
+}
+
+export default spotsReducer
