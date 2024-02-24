0a5ec55bf69080a6674cff96eca2baa5102c2779
Ryou <nishiyamaryou@gmail.com>
2024-02-24 03:47:23 -0500

    4components left to style

--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -24,47 +24,56 @@ function LoginFormModal() {
       });
   };
 
-  const demoUser = (e) =>{
+  const demoUser = (e) => {
     e.preventDefault()
     setErrors({})
-    return dispatch(sessionActions.login({ credential: 'demo@user.io', password: 'password'}))
-    .then(closeModal)
-    .catch(async (res) => {
-      const data = await res.json()
-      if (data && data.errors) {
-        setErrors(data.errors)
-      }
-    })
+    return dispatch(sessionActions.login({ credential: 'demo@user.io', password: 'password' }))
+      .then(closeModal)
+      .catch(async (res) => {
+        const data = await res.json()
+        if (data && data.errors) {
+          setErrors(data.errors)
+        }
+      })
   }
 
   return (
     <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username or Email
-          <input
-            type="text"
-            value={credential}
-            onChange={(e) => setCredential(e.target.value)}
-            required
-          />
-        </label>
-        {errors.credential && <p className='errors'>{errors.credential}</p>}
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.credential && <p className='errors'>{errors.password}</p>}
-        <button type="submit" disabled={credential.length<4 || password.length <6}>Log In</button>
-        <button type='submit' onClick={demoUser}>Log in as Demo User</button>
-      </form>
-
+      <div className='Loginform-container'>
+        <h1 className='Loginform-title'>Log In</h1>
+        <form className='Loginform' onSubmit={handleSubmit}>
+          <div>
+            <input
+              className='Login-inputfield'
+              type="text"
+              value={credential}
+              placeholder='Username or Email'
+              onChange={(e) => setCredential(e.target.value)}
+              required
+            />
+          </div>
+          <div className='Errors-text'>
+            {errors.credential && <p className='errors'>{errors.credential}</p>}
+          </div>
+          <div>
+            <input
+              className='Login-inputfield'
+              type="password"
+              placeholder='Password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <div className='Errors-text'>
+              {errors.credential && <p className='errors'>{errors.password}</p>}
+            </div>
+          </div>
+          <div className='Login-buttons'>
+            <button type="submit" disabled={credential.length < 4 || password.length < 6}>Log In</button>
+            <span className='Login-demo' type='submit' onClick={demoUser}>Log in as Demo User</span>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
